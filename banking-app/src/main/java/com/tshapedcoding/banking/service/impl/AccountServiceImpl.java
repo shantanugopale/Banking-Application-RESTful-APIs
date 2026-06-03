dfab57036ccb4740a2e0ea32983f9d1e931a0cde
Shantanu Gopale <shantanu.gopale@novillex.local>
2026-06-03 18:08:07 +0530

    removed line from impl file

--- banking-app/src/main/java/com/tshapedcoding/banking/service/impl/AccountServiceImpl.java
+++ banking-app/src/main/java/com/tshapedcoding/banking/service/impl/AccountServiceImpl.java
@@ -59,7 +59,6 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public List<AccountDto> getAllAccounts() {
-        List<Account> accounts = accountRepository.findAll();
         return accounts.stream().map((account)->AccountMapper.mapToAccountDto(account)).collect(Collectors.toList());
     }
 
